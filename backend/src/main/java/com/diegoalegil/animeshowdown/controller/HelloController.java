670c995418a5269a2e00ac93dfaef1696e1984c6
diegoalegil <diegogildam@gmail.com>
2026-05-07 00:24:38 +0100

    feat: primer endpoint REST funcional con conexión a PostgreSQL

---
+++ backend/src/main/java/com/diegoalegil/animeshowdown/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.diegoalegil.animeshowdown.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api")
+
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello () {
+        return "Hola, AnimeShowdown esta vivo!!!";
+    }
+
+}
