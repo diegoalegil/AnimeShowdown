14c5744d5406630da150b33badba69fddb51d200
diegoalegil <diegogildam@gmail.com>
2026-05-07 16:34:27 +0100

    feat: añade endpoint POST /api/personajes y card de Tsuyu Asui

--- backend/src/main/java/com/diegoalegil/animeshowdown/controller/PersonajeController.java
+++ backend/src/main/java/com/diegoalegil/animeshowdown/controller/PersonajeController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +38,11 @@ public class PersonajeController {
         } else{
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    public Personaje crear (@RequestBody Personaje personaje){
+        return personajeRepository.save(personaje);
     }
     
 }
