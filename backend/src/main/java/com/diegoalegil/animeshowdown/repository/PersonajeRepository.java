11c6f5d79119f8f46462ede7e2636281c84ed984
diegoalegil <diegogildam@gmail.com>
2026-05-07 21:35:18 +0100

    feat: añade filtro por anime con @RequestParam y findByAnime

--- backend/src/main/java/com/diegoalegil/animeshowdown/repository/PersonajeRepository.java
+++ backend/src/main/java/com/diegoalegil/animeshowdown/repository/PersonajeRepository.java
@@ -2,6 +2,9 @@ package com.diegoalegil.animeshowdown.repository;
 
 import com.diegoalegil.animeshowdown.model.Personaje;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PersonajeRepository extends JpaRepository<Personaje, Long> {
+
+    List<Personaje> findByAnime(String anime);
 }
