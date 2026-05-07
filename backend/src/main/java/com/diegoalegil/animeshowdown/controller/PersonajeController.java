c8fa93992855f723ba38f3a64222ec12c11b004a
diegoalegil <diegogildam@gmail.com>
2026-05-07 17:05:43 +0100

    feat: completa CRUD con endpoints PUT y DELETE con manejo de 404

--- backend/src/main/java/com/diegoalegil/animeshowdown/controller/PersonajeController.java
+++ backend/src/main/java/com/diegoalegil/animeshowdown/controller/PersonajeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.diegoalegil.animeshowdown.model.Personaje;
 import com.diegoalegil.animeshowdown.repository.PersonajeRepository;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/api/personajes")
@@ -43,6 +46,26 @@ public class PersonajeController {
     @PostMapping
     public Personaje crear (@RequestBody Personaje personaje){
         return personajeRepository.save(personaje);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id){
+
+        if (!personajeRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        personajeRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Personaje> actualizar (@PathVariable Long id, @RequestBody Personaje datos){
+        if (!personajeRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        datos.setId(id);
+        Personaje actualizado = personajeRepository.save(datos);
+        return ResponseEntity.ok(actualizado);
     }
     
 }
